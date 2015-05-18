3b1e2b6b256070adfa875d7a02bc01bd0b6f5cf9
Rohith Premachandran <rohith@inbetween.com>
2015-05-18 20:21:04 +0530

    create image in container api

--- scripts/inhousescripts/eventHandler.js
+++ scripts/inhousescripts/eventHandler.js
@@ -27,10 +27,27 @@ function attachEventsOnElement () {
                                  });
 }
 
-function createTextEditorInContainer($element) {
+function createTextEditorInContainer($element){
 
-  var $newEditorDiv = $('<div class="right-container-dropped-text-field">');
-  $element.append($newEditorDiv);
+    var $newEditorDiv = $('<div class="right-container-dropped-text-field">');
+    $newEditorDiv.editable({inlineMode:false});
 
-  $newEditorDiv.editable({inlineMode: false});
+    $element.append($newEditorDiv);
+
+}
+
+function createImageInsertInContainer($element){
+
+    $element.append(getImageInsert());
+
+}
+
+function getImageInsert(){
+
+    var $newImageContainer = $('<div class="right-container-dropped-image-container">');
+    var $addImageButton = $('<div class="insert-image-button">');
+
+    $newImageContainer.append($addImageButton);
+
+    return $newImageContainer;
 }
